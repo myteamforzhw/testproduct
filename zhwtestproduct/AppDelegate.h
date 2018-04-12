c30b5566be76071125d1e1f8b2856c4374cb38ce
hongwei.zhang <hongwei.zhang@jrj.com.cn>
2018-04-12 15:06:40 +0800

    Initial Commit

---
+++ zhwtestproduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhwtestproduct
+//
+//  Created by zhw on 2018/4/12.
+//  Copyright © 2018年 aitougu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhwtestproduct/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhwtestproduct
+//
+//  Created by zhw on 2018/4/12.
+//  Copyright © 2018年 aitougu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
